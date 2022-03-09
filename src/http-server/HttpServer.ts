4b42331f927da19f03b76166c692f43d9699b490
Pierre-Luc Millet <pierre-luc.millet@student.junia.com>
2022-03-09 13:47:58 +0100

    adding allow control origin

--- src/http-server/HttpServer.ts
+++ src/http-server/HttpServer.ts
@@ -13,10 +13,12 @@ export class HttpServer{
         this.app.use(express.urlencoded({
             extended: false
         }));
-        this.app.use(cors({
-            origin: 'http://localhost:1234/',
-            optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-        }));
+        this.app.use(cors());
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                next();
+        })
         this.app.use(cookieParser());
     }
     routes(pathRoute:string,routingModule:express.Router):void{
