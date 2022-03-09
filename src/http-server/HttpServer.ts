9c518dd529ace0ce52c0259c8a4a0fa51dde6a3a
Pierre-Luc Millet <pierre-luc.millet@student.junia.com>
2022-03-09 13:52:40 +0100

    test

--- src/http-server/HttpServer.ts
+++ src/http-server/HttpServer.ts
@@ -13,11 +13,7 @@ export class HttpServer{
         this.app.use(express.urlencoded({
             extended: false
         }));
-        this.app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-                next();
-        })
+   
         this.app.use(cookieParser());
     }
     routes(pathRoute:string,routingModule:express.Router):void{
