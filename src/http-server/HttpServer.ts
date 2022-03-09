9c2402ffe7087672c1cfff7b1e54d6ea3bde1635
Pierre-Luc Millet <pierre-luc.millet@student.junia.com>
2022-03-09 13:29:14 +0100

    adding cors options

--- src/http-server/HttpServer.ts
+++ src/http-server/HttpServer.ts
@@ -13,7 +13,10 @@ export class HttpServer{
         this.app.use(express.urlencoded({
             extended: false
         }));
-        this.app.use(cors());
+        this.app.use(cors({
+            origin: '*',
+            optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+        }));
         this.app.use(cookieParser());
     }
     routes(pathRoute:string,routingModule:express.Router):void{
